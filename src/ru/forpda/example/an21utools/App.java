53d174f414aac672f9cab6f396bc47ed3a6392b8
max314 <max314@mail.ru>
2014-11-07 09:44:59 +0300

    Повесили обработку ошибок на уровне приложения

--- src/ru/forpda/example/an21utools/App.java
+++ src/ru/forpda/example/an21utools/App.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import ru.forpda.example.an21utools.util.LogHelper;
 
 /**
+ * Приложение
  * Created by max on 30.10.2014.
  */
 public class App extends Application {
@@ -14,6 +15,13 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread thread, Throwable ex) {
+                Log.e("Unhandled ", ex);
+            }
+        });
+
         Log.d("Application start");
     }
 }
